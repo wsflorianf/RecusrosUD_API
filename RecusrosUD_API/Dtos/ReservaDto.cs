c1b3f4cfa43d0c694ee1bb85c8a25f338ab87b57
Santiago Florian <floranwsfrench@gmail.com>
2025-02-04 12:47:20 -0500

    Dto de Reserva agregado

---
+++ RecusrosUD_API/Dtos/ReservaDto.cs
@@ -0,0 +1,19 @@
+﻿namespace RecusrosUD_API.Dtos
+{
+    public class ReservaDto
+    {
+        public long Id { get; set; }
+
+        public string IdentificadorRecurso { get; set; } = null!;
+
+        public string NombreRecurso { get; set; } = null!;
+
+        public string NombreUsuario { get; set; } = null!;
+
+        public DateOnly Fecha { get; set; }
+
+        public TimeSpan HoraInicio { get; set; }
+
+        public TimeSpan HoraFin { get; set; }
+    }
+}
